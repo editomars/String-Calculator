2b58f0cc460519582f0b2b0bdc7b34917f173a53
editomars <editomars@gmail.com>
2017-10-09 20:13:01 +0000

    ex. 3, testing new line and added test for new line with commas

--- src/test/java/is/ru/hugb/CalculatorTest.java
+++ src/test/java/is/ru/hugb/CalculatorTest.java
@@ -35,13 +35,4 @@ public class CalculatorTest {
 		assertEquals(6, StringCalculator.add("1,2\n3"));
 	}
 	
-	/*@Rule
-	public ExpectedException exception = ExpectedException.none();
-
-	@Test
-	public void testNegatives(){
-		exception.expect(IllegalArgumentException.class);
-		exception.expectMessage("Negatives not allowed: -1");
-		StringCalculator.add("-1,2");		
-	}*/
 }
